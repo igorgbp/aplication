da1c9818834b253da31bcce2554e9a0e2e212b92
IGOR GABRIEL BARBOSA PEREIRA <0041933@ifmg.edu.br>
2022-09-14 10:54:09 -0300

    adição do exlui e o estilo css

---
+++ access/seeUserData.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+        define('_BASE', $_SERVER['DOCUMENT_ROOT'] . '/aplication/');
+
+        require_once _BASE . 'dao/conexao.php';
+        require_once _BASE . 'model/users.php';
+        require_once _BASE . 'dao/DaoUsers.php';
+
+
+
+
+
+
+        // LISTAR
+        $du = new DaoUsers();
+        $list = $du->list();
+        echo '<table border="1">';
+        echo '<tr>';
+        echo '<th>id </th>';
+        echo '<th>nome </th>';
+        echo '<th>sexo </th>';
+        echo '<th>email </th>';
+        echo '<th>ações </th>';
+        echo '</tr>';
+
+        foreach ($list as $indice => $valor) {
+            echo '<tr>';
+            foreach ($valor as $item => $dado) {
+                echo '<td>';
+                echo ($dado);
+                echo '</td>';
+            }
+            // echo ($valor);
+            echo ' <td> <a href = "deleteUserData.php?codigo=' . $valor['id'] . '">excluir</a></td>';
+            echo '</tr>';
+        }
+
+        // foreach($list as $indice){
+        //     echo '<tr>';
+        //         echo 'asdf';
+        //     echo '</tr>';
+        // }
+
+        echo '</table>';
+
+        ?>
+
+
+
+
+
+
+    </h1>
+    <p>
+        <a href='../new.php'>Voltar para cadastro</a>
+    </p>
+
+</body>
+
+</html>
